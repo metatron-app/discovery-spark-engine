422c87d91ddd3b48e559aed393e8f361b90f2f1e
Kim, Joo-Ho <teamsprint@gmail.com>
2019-07-05 12:47:57 +0900

    Implementing settype rule. LONG test done.

--- discovery-prep-spark-engine/src/test/java/app/metatron/discovery/prep/spark/rest/SetTypeTest.java
+++ discovery-prep-spark-engine/src/test/java/app/metatron/discovery/prep/spark/rest/SetTypeTest.java
@@ -96,15 +96,27 @@ public class SetTypeTest {
   }
 
   @Test
+  public void testSetTypeDouble() {
+    List<String> ruleStrings = new ArrayList();
+
+    ruleStrings.add("header rownum: 1");
+    ruleStrings.add("settype col: `sale_price` type: Double");
+
+    String dsPath = "/tmp/dataprep/uploads/sales_named.csv";
+    String ssPath = "/tmp/dataprep/snapshots/crime.snapshot.csv";
+
+    testCsvToCsv(dsPath, ruleStrings, ssPath);
+  }
+
+  @Test
   public void testSetTypeDate() {
     List<String> ruleStrings = new ArrayList();
 
     ruleStrings.add("header rownum: 1");
-    ruleStrings.add(
-        "rename col: `Population_`, `Murder_`, `Forcible_Rape_`, `Robbery_`, `Aggravated_Assault_`, `Burglary_`, `Larceny_Theft_`, `Vehicle_Theft_` to: `Population`, `Murder`, `Forcible_Rape`, `Robbery`, `Aggravated_Assault`, `Burglary`, `Larceny_Theft`, `Vehicle_Theft`");
-    ruleStrings.add("settype col: `Date` type: Timestamp format: 'yyyy.MM.dd'");
+    ruleStrings
+        .add("settype col: `contract_date` type: Timestamp format: 'yyyy-MM-dd\'T\'HH:mm:ssz'");
 
-    String dsPath = "/tmp/dataprep/uploads/crime.csv";
+    String dsPath = "/tmp/dataprep/uploads/sales_named.csv";
     String ssPath = "/tmp/dataprep/snapshots/sales.snapshot.csv";
 
     testCsvToCsv(dsPath, ruleStrings, ssPath);
